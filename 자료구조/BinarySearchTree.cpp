e1474a5f7ab5e13a7a15e176420e64a525c3a0c1
명진 김 <acws12@nate.com>
2021-10-25 11:08:09 +0900

    Bst

--- 자료구조/BinarySearchTree.cpp
+++ 자료구조/BinarySearchTree.cpp
@@ -41,12 +41,32 @@ void MakeRightSubTree(BTreeNode* main, BTreeNode* sub) {
 	main->right = sub;
 }
 
-void RemoveLeftSubTree(BTreeNode* bt) {
+// 왼쪽 자식 노드 제거, 제거된 노드의 주소 값이 반환된다.
+BTreeNode* RemoveLeftSubTree(BTreeNode* bt) {
+	BTreeNode* delNode;
+	
+	delNode = bt->left;
 	bt->left = NULL;
+	return delNode;
 }
 
-void RemoveRightSubTree(BTreeNode* bt) {
+// 오른쪽 자식 노드 제거, 제거된 노드의 주소 값이 반환된다.
+BTreeNode* RemoveRightSubTree(BTreeNode* bt) {
+	BTreeNode* delNode;
+	
+	delNode = bt->right;
 	bt->right = NULL;
+	return delNode;
+}
+
+// 메모리 소멸을 수반하지 않고 main의 왼쪽 자식 노드를 변경한다. 
+void ChangeLeftSubTree(BTreeNode* main, BTreeNode* sub) {
+	main->left = sub;
+}
+
+// 메모리 소멸을 수반하지 않고 main의 오른쪽 자식 노드를 변경한다. 
+void ChangeRightSubTree(BTreeNode* main, BTreeNode* sub) {
+	main->right = sub;
 }
 
 typedef void VisitFuncPtr(BTData data);
@@ -104,25 +124,86 @@ BTreeNode* BSTSearch(BTreeNode* bst, BSTData target) {
 	return NULL;
 }
 
-void PreorderTraverse(BTreeNode* bt, VisitFuncPtr action) {
-	if (bt == NULL)return;
-	action(bt->data);
-	PreorderTraverse(bt->left, action);
-	PreorderTraverse(bt->right, action);
-}
-void InorderTraverse(BTreeNode* bt, VisitFuncPtr action) {
-	if (bt == NULL)return;	
-	PreorderTraverse(bt->left, action);
-	action(bt->data);
-	PreorderTraverse(bt->right, action);
-}
-void PostorderTraverse(BTreeNode* bt, VisitFuncPtr action) {
-	if (bt == NULL)return;
-	PreorderTraverse(bt->left, action);	
-	PreorderTraverse(bt->right, action);
-	action(bt->data);
+// 트리에서 노드를 제거하고 제거된 노드의 주소 값을 반환한다. 
+BTreeNode* BSTRemove(BTreeNode** pRoot, BSTData target) {
+	// 삭제 대상이 루트 노드인 경우를 별도로 고려해야 한다.
+	BTreeNode* pVRoot = MakeBTreeNode(); // 가상 루트 노드
+
+	BTreeNode* pNode = pVRoot; // parent node
+	BTreeNode* cNode = *pRoot; // current node
+	BTreeNode* dNode; // delete node;
+
+	//루트 노드를 pVRoot가 가르키는 노드의 오른쪽 서브 노드가 되게 한다.
+	ChangeRightSubTree(pVRoot, *pRoot);
+
+	//삭제 대상을 지정한 노드 탐색
+	while (cNode != NULL && GetData(cNode) != target) {
+		pNode = cNode;
+		//target이 부모노드보다 작다면 왼쪽 서브트리의 주소를 저장
+		if (target < GetData(cNode)) cNode = GetLeftSubTree(cNode);
+		else cNode = GetRightSubTree(cNode);
+	}
+	if (cNode == NULL) return NULL; // 삭제 대상이 존재하지 않을 경우
+	
+	dNode = cNode; // 삭제대상을 가르킴
+	// 첫 번째 경우 - 삭제할 노드가 단말 노드인 경우
+	if (GetLeftSubTree(dNode) == NULL && GetRightSubTree(dNode) == NULL) {
+		if (GetLeftSubTree(pNode) == dNode) RemoveLeftSubTree(pNode);
+		else RemoveRightSubTree(pNode);
+	}
+	// 두 번째 경우 - 하나의 자식 노드를 갖는 경우
+	else if (GetLeftSubTree(dNode) == NULL || GetRightSubTree(dNode) == NULL) {
+		BTreeNode* dcNode; //deltet node의 자식 노드
+		//삭제 대상의 자식 노드를 찾는다.
+		if (GetLeftSubTree(dNode) != NULL) dcNode = GetLeftSubTree(dNode);
+		else dcNode = GetRightSubTree(dNode);
+		// 삭제 대상의 부모 노드와 자식 노드를 연결한다.
+		if (GetLeftSubTree(pNode) == dNode) // 삭제 대상이 왼쪽 자식 노드
+			ChangeLeftSubTree(pNode, dcNode);
+		else ChangeRightSubTree(pNode, dcNode); // 삭제 대상이 오른쪽 자식 노드
+	}
+	// 세 번째 경우 - 두 개의 자식 노드를 모두 갖는 경우
+	else {
+		BTreeNode* mNode = GetRightSubTree(dNode); // min node, 대체노드를 가르킴
+		BTreeNode* mpNode = dNode; // min node의 부모 노드, 대체노드의 부모노드
+		int delData;
+		// step1. 삭제 노드를 대체할 노드를 찾음
+		while (GetLeftSubTree(mNode) != NULL) {
+			mpNode = mNode;
+			mNode = GetLeftSubTree(mNode);
+		}
+		// step2. 대체할 노드에 저장된 값을 삭제할 노드에 대입
+		delData = GetData(dNode);
+		SetData(dNode, GetData(mNode));
+		// step3. 대체할 노드의 부모 노드와 자식 노드를 연결
+		if (GetLeftSubTree(mpNode) == mNode) ChangeLeftSubTree(mpNode, GetRightSubTree(mNode));
+		else ChangeRightSubTree(mpNode, GetRightSubTree(mNode));
+
+		dNode = mNode;
+		SetData(dNode, delData); // 백업 데이터 복원
+	}
+	// 삭제된 노드가 루트 노드인 경우를 처리
+	if (GetRightSubTree(pVRoot) != *pRoot) *pRoot = GetRightSubTree(pVRoot);
+	free(pVRoot);
+	return dNode;
 }
 
+void ShowData(int data) {
+	printf("%d ", data);
+}
+
+void InorderTraverse(BTreeNode* bt, VisitFuncPtr action) {
+	if (bt == NULL)return;	
+	InorderTraverse(bt->left, action);
+	action(bt->data);
+	InorderTraverse(bt->right, action);
+}
+
+
+// 이진 탐색 트리에 저장된 모든 노드의 데이터를 출력한다.
+void BSTShowAll(BTreeNode* bst) {
+	InorderTraverse(bst, ShowData);
+}
 
 int main() {
 	BTreeNode* bstRoot;
@@ -134,13 +215,27 @@ int main() {
 		scanf("%d", &val);
 		BSTInsert(&bstRoot, val);
 	}
+	BSTShowAll(bstRoot); printf("\n"); printf("\n");
 	int M;
 	scanf("%d", &M);
-	for (int val, i = 0; i < M; i++) {
+	/*for (int val, i = 0; i < M; i++) {
 		scanf("%d", &val);
 		sNode = BSTSearch(bstRoot, val);
 		if (sNode == NULL)printf("탐색 실패\n");
 		else printf("key : %d\n", BSTGetNodeData(sNode));
+	}*/
+	for (int val,i = 0; val,i < M; val,i++)
+	{
+		scanf("%d", &val);
+		sNode = BSTRemove(&bstRoot, val);
+		free(sNode);
+		BSTShowAll(bstRoot); printf("\n");
 	}
 	return 0;
 }
+/*
+9
+5 8 1 6 4 9 3 2 7
+3
+7 1 4
+*/
